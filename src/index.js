116ae2b78e947e1a79f54f5fd3101fa3c26a536c
luiznaiper <luiz_naiper@hotmail.com>
2022-05-12 18:57:42 -0300

    Consuming categories preview

--- src/index.js
+++ src/index.js
@@ -23,30 +23,30 @@ async function getTrendingMoviesPreview(){
         trendingPreviewMoviesContainer.appendChild(movieContainer)
     });   
 }
-getTrendingMoviesPreview()
+
 
 async function getCategoriesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY)
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`) 
     const data = await res.json()
 
-    const movies = data.results
-    console.log({data, movies})
-    movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const categories = data.genres
+    console.log({data, categories})
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
-        const movieContainer = document.createElement('div')
-        movieContainer.classList.add('movie-container')
+        const categoryContainer = document.createElement('div')
+        categoryContainer.classList.add('category-container')
 
-        const movieImg = document.createElement('img')
-        movieImg.classList.add('movie-img')
-        movieImg.setAttribute('alt', movie.title)
-        movieImg.setAttribute(
-        'src', 
-        `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
-        )
+        const categoryTitle = document.createElement('h3')
+        categoryTitle.classList.add('category-title')
+        categoryTitle.setAttribute('id', `id${category.id}`)
+        const categoryTitleText = document.createTextNode(category.name)
 
-        movieContainer.appendChild(movieImg)
-        trendingPreviewMoviesContainer.appendChild(movieContainer)
+        categoryTitle.appendChild(categoryTitleText)
+        categoryContainer.appendChild(categoryTitle)
+        previewCategoriesContainer.appendChild(categoryContainer)
     });   
 }
+
+getTrendingMoviesPreview()
 getCategoriesPreview()
